f4a2f375a07e57467b30f4d80b06f77012264a50
Lawrence <Caulker>
2020-02-13 17:38:08 -0500

    completed props

--- src/index.js
+++ src/index.js
@@ -1,54 +1,45 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import faker from 'faker'
+import CommentDetail from './CommentDetail'
+import ApprovalCard from './ApprovalCard'
 
-const App = () => {
+const App = () =>
+{
   return (
     <div className="ui container comments">
 
-      <div className="comment">
-        <a href="/" className="avatar">
-          <img src={faker.image.avatar()} alt="avatar" />
-        </a>
-        <div className="content">
-          <a href="/" className="author">
-            Sam
-          </a>
-          <div className="metadata">
-            <span className="date">Today at 6:00PM</span>
-          </div>
-          <div className="text">{faker.lorem.sentence()}</div>
-        </div>
-      </div>
-      <div className="comment">
-        <a href="/" className="avatar">
-          <img src={faker.image.avatar()} alt="avatar" />
-        </a>
-        <div className="content">
-          <a href="/" className="author">
-            Sam
-          </a>
-          <div className="metadata">
-            <span className="date">Today at 6:00PM</span>
-          </div>
-          <div className="text">{faker.lorem.sentence()}</div>
-        </div>
-      </div>
-      <div className="comment">
-        <a href="/" className="avatar">
-          <img src={faker.image.avatar()} alt="avatar" />
-        </a>
-        <div className="content">
-          <a href="/" className="author">
-            Sam
-          </a>
-          <div className="metadata">
-            <span className="date">Today at 6:00PM</span>
-          </div>
-          <div className="text">{faker.lorem.sentence()}</div>
-        </div>
-      </div>
+      <ApprovalCard>
+        <h3>Demo Child Components</h3>
+        <p>We believe that to date a social order has been formed for the general theory of advertising.</p>
+      </ApprovalCard>
 
+      <ApprovalCard>
+        <CommentDetail
+          author="Sam"
+          timeAgo="Today at 4:45 PM"
+          content={faker.lorem.sentence()}
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail
+          author="Alex"
+          timeAgo="Today at 2:00 AM"
+          content={faker.lorem.sentence()}
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail
+          author="Jane"
+          timeAgo="Yesterday at 5:00 PM"
+          content={faker.lorem.sentence()}
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
     </div>
   )
 }
