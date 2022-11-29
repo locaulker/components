dfb1cee7ca73b9a282d40175708318fda589ba8d
Lawrence <locaulker@gmail.com>
2022-11-29 01:52:31 -0500

    installed TailwindCSS

--- src/Button.js
+++ src/Button.js
@@ -1,7 +1,34 @@
 import React from "react"
+import PropTypes from "prop-types"
 
-function Button({ children }) {
+function Button({
+  children,
+  primary,
+  secondary,
+  success,
+  warning,
+  danger,
+  outline,
+  rounded
+}) {
   return <button>{children}</button>
+}
+
+Button.propTypes = {
+  checkVariationValue: ({ primary, secondary, success, warning, danger }) => {
+    const count =
+      Number(!!primary) +
+      Number(!!secondary) +
+      Number(!!success) +
+      Number(!!warning) +
+      Number(!!danger)
+
+    if (count > 1) {
+      return new Error(
+        "Only one of primary, secondary, success, warning or danger is allowed."
+      )
+    }
+  }
 }
 
 export default Button
