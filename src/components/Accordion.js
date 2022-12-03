2ff79946e5def58a95b73699fd30b6e7d6c364d5
Lawrence O. Caulker <locaulker@gmail.com>
2022-12-02 20:49:57 -0500

    added styling

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,24 +1,35 @@
 import React, { useState } from "react"
+import { GoChevronDown, GoChevronLeft } from "react-icons/go"
 
 function Accordion({ items }) {
-  const [expandedIndex, setExpandedIndex] = useState(0)
+  const [expandedIndex, setExpandedIndex] = useState(-1)
 
-  const handleClick = index => {
-    setExpandedIndex(index)
+  const handleClick = nextIndex => {
+    setExpandedIndex(nextIndex)
   }
 
   const renderedItems = items.map((item, index) => {
     const isExpanded = index === expandedIndex
 
+    const icon = (
+      <span>{isExpanded ? <GoChevronDown /> : <GoChevronLeft />}</span>
+    )
+
     return (
       <div key={item.id}>
-        <div onClick={() => handleClick(index)}>{item.label}</div>
-        {isExpanded && <div>{item.content}</div>}
+        <div
+          className="flex p-3 bg-gray-50 border-b cursor-pointer"
+          onClick={() => handleClick(index)}
+        >
+          {item.label}
+          {icon}
+        </div>
+        {isExpanded && <div className="border-b p-5">{item.content}</div>}
       </div>
     )
   })
 
-  return <div>{renderedItems}</div>
+  return <div className="border-x border-t rounded">{renderedItems}</div>
 }
 
 export default Accordion
