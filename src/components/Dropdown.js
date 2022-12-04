131c66ce645d6859fd08c5d8142a759b8dc8a864
Lawrence O. Caulker <locaulker@gmail.com>
2022-12-04 05:27:16 -0500

    implemented select option, Resume @ L194

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,7 +1,22 @@
-import React from "react"
+import React, { useState } from "react"
 
 function Dropdown({ options }) {
-  return <div>Dropdown...</div>
+  const [isOpen, setIsOpen] = useState(false)
+
+  const renderedOptions = options.map(option => {
+    return <div key={option.value}>{option.label}</div>
+  })
+
+  const handleClick = () => {
+    setIsOpen(!isOpen)
+  }
+
+  return (
+    <div>
+      <div onClick={handleClick}>Select...</div>
+      {isOpen && <div>{renderedOptions}</div>}
+    </div>
+  )
 }
 
 export default Dropdown
