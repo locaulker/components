3af831ef1079d5fafc3a393a69941bc2ee838af0
Lawrence <locaulker@gmail.com>
2022-12-06 01:53:23 -0500

    resume @ L209

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,9 +1,18 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 import { GoChevronDown } from "react-icons/go"
-import { GoChevronLeft } from "react-icons/go"
+// import { GoChevronLeft } from "react-icons/go"
+import Panel from "./Panel"
 
 function Dropdown({ options, value, onChange }) {
   const [isOpen, setIsOpen] = useState(false)
+
+  useEffect(() => {
+    const handler = event => {
+      console.log(event.target)
+    }
+
+    document.addEventListener("click", handler, true)
+  }, [])
 
   const handleClick = () => {
     setIsOpen(!isOpen)
@@ -41,18 +50,14 @@ function Dropdown({ options, value, onChange }) {
    */
   return (
     <div className="w-48 relative">
-      <div
-        className="flex justify-between items-center cursor-pointer border rounded p-3 shadow bg-white w-full"
+      <Panel
+        className="flex justify-between items-center cursor-pointer"
         onClick={handleClick}
       >
         {value?.label || "Select..."}
         <GoChevronDown className="text-xl" />
-      </div>
-      {isOpen && (
-        <div className="absolute top-full border rounded p-3 shadow bg-white w-full">
-          {renderedOptions}
-        </div>
-      )}
+      </Panel>
+      {isOpen && <Panel className="absolute top-full">{renderedOptions}</Panel>}
     </div>
   )
 }
