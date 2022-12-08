e1c69b319fe494c71a7523e10e4adbc69170fbe1
Lawrence O. Caulker <locaulker@gmail.com>
2022-12-08 08:31:58 -0500

    refactored to use custom hooks

--- src/components/Link.js
+++ src/components/Link.js
@@ -1,9 +1,8 @@
 import classNames from "classnames"
-import React, { useContext } from "react"
-import NavigationContext from "../context/navigation"
+import useNavigation from "../hooks/use-navigation"
 
 function Link({ to, children }) {
-  const { navigate } = useContext(NavigationContext)
+  const { navigate } = useNavigation()
 
   const classes = classNames("text-blue-500")
 
