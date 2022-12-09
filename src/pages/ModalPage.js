5ebd147115ebce78994c46848fe5f150cb4324d2
Lawrence <locaulker@gmail.com>
2022-12-09 05:47:53 -0500

    resume @L237

--- src/pages/ModalPage.js
+++ src/pages/ModalPage.js
@@ -1,10 +1,38 @@
-import React from "react"
+import React, { useState } from "react"
 import Modal from "../components/Modal"
+import Button from "../components/Button"
 
 function ModalPage() {
+  const [showModal, setShowModal] = useState(false)
+
+  const handleClick = () => {
+    setShowModal(true)
+  }
+
+  const handleClose = () => {
+    setShowModal(false)
+  }
+
+  const actionBar = (
+    <div>
+      <Button onClick={handleClose} primary>
+        I Accept
+      </Button>
+    </div>
+  )
+
+  const modal = (
+    <Modal onClose={handleClose} actionBar={actionBar}>
+      <p>Here is an important agreement for you to accept.</p>
+    </Modal>
+  )
+
   return (
     <div>
-      <Modal />
+      <Button onClick={handleClick} primary>
+        Open Modal
+      </Button>
+      {showModal && modal}
     </div>
   )
 }
