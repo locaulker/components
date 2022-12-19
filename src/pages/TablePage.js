675f7173f9374b62d624bde2bc07c48dca566f7c
Lawrence <locaulker@gmail.com>
2022-12-19 06:32:19 -0500

    end sortable table component

--- src/pages/TablePage.js
+++ src/pages/TablePage.js
@@ -7,7 +7,8 @@ function TablePage() {
     { name: "Orange", color: "bg-orange-500", score: 5 },
     { name: "Apple", color: "bg-red-500", score: 3 },
     { name: "Banana", color: "bg-yellow-500", score: 1 },
-    { name: "Lime", color: "bg-green-500", score: 4 }
+    { name: "Lime", color: "bg-green-500", score: 4 },
+    { name: "Cherry", color: "bg-red-700", score: 2.5 }
   ]
 
   const config = [
@@ -24,6 +25,11 @@ function TablePage() {
       label: "Score",
       render: fruit => fruit.score,
       sortValue: fruit => fruit.score
+    },
+    {
+      label: "Score Squared",
+      render: fruit => fruit.score ** 2,
+      sortValue: fruit => fruit.score ** 2
     }
   ]
 
@@ -33,6 +39,7 @@ function TablePage() {
 
   return (
     <div>
+      {/* <Table data={data} config={config} keyFn={keyFn} /> */}
       <SortableTable data={data} config={config} keyFn={keyFn} />
     </div>
   )
