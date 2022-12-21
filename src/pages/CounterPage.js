8aafec8d740edebcc4aa2849a82ea421089797ac
Lawrence <locaulker@gmail.com>
2022-12-21 01:18:03 -0500

    basic implementation of the reducer function

--- src/pages/CounterPage.js
+++ src/pages/CounterPage.js
@@ -3,7 +3,10 @@ import Button from "../components/Button"
 import Panel from "../components/Panel"
 
 const reducer = (state, action) => {
-  //
+  return {
+    ...state,
+    count: state.count + 1
+  }
 }
 
 function CounterPage({ initialCount }) {
@@ -11,12 +14,12 @@ function CounterPage({ initialCount }) {
   // const [valueToAdd, setValueToAdd] = useState(0)
   const [state, dispatch] = useReducer(reducer, {
     count: initialCount,
-    valueToAdd: 0,
+    valueToAdd: 0
   })
   console.log(state)
 
   const increment = () => {
-    // setCount(count + 1)
+    dispatch()
   }
   const decrement = () => {
     // setCount(count - 1)
@@ -39,8 +42,8 @@ function CounterPage({ initialCount }) {
     <Panel className="m-3">
       <h1 className="text-lg">Count is {state.count}</h1>
       <div className="flex flex-row">
-        <Button onClick={decrement}>Decrement</Button>
         <Button onClick={increment}>Increment</Button>
+        <Button onClick={decrement}>Decrement</Button>
       </div>
 
       <form onSubmit={handleSubmit}>
