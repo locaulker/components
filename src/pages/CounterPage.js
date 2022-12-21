6fc4c7bf13c6164569d0d6c6291591ef5193c9c5
Lawrence O. Caulker <locaulker@gmail.com>
2022-12-21 13:08:45 -0500

    implemented variable constants

--- src/pages/CounterPage.js
+++ src/pages/CounterPage.js
@@ -2,15 +2,18 @@ import { useReducer } from "react"
 import Button from "../components/Button"
 import Panel from "../components/Panel"
 
+const INCREMENT_COUNT = "increment"
+const SET_VALUE_TO_ADD = "set-value-to-add"
+
 const reducer = (state, action) => {
-  if (action.type === "increment") {
+  if (action.type === INCREMENT_COUNT) {
     return {
       ...state,
       count: state.count + 1,
     }
   }
 
-  if (action.type === "change-value-to-add") {
+  if (action.type === SET_VALUE_TO_ADD) {
     return {
       ...state,
       valueToAdd: action.payload,
@@ -31,7 +34,7 @@ function CounterPage({ initialCount }) {
 
   const increment = () => {
     dispatch({
-      type: "increment",
+      type: INCREMENT_COUNT,
     })
   }
   const decrement = () => {
@@ -42,7 +45,7 @@ function CounterPage({ initialCount }) {
     const value = parseInt(event.target.value) || 0
 
     dispatch({
-      type: "change-value-to-add",
+      type: SET_VALUE_TO_ADD,
       payload: value,
     })
   }
